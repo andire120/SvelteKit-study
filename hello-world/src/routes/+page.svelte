3b23d4ae668019da4010cad3e23e2213ce01e69a
andire120 <164429441+andire120@users.noreply.github.com>
2024-11-07 19:18:38 +0900

    dsfz

--- hello-world/src/routes/+page.svelte
+++ hello-world/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Hello World</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome home</h1>
+
+<a href = "/about">About</a>
+<a href = "/profile">Profile</a>
